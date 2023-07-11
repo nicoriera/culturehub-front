d4c048a4ceb7e2611722fb9691ac40dc5d1bc0f9
nicolas riera <nicolariera@gmail.com>
2023-07-11 16:41:00 +0200

    feat page admin with component adminarticles and creatarticleform

--- src/components/CreateArticleForm/CreateArticleForm.js
+++ src/components/CreateArticleForm/CreateArticleForm.js
@@ -47,72 +47,81 @@ const CreateArticleForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Titre:
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Contenu:
-        <textarea
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Catégorie:
-        <select
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-          required
-        >
-          <option value="news">News</option>
-          <option value="musique">Musique</option>
-          <option value="cinema">Cinéma</option>
-          <option value="serie">Série</option>
-          <option value="jeuxvideo">Jeux vidéo</option>
-          <option value="hightech">High Tech</option>
-        </select>
-      </label>
-      <label>
-        Image:
-        <input
-          type="file"
-          accept="image/*"
-          onChange={(e) => setImage(e.target.files[0])}
-          required
-        />
-      </label>
-      <label>
-        Auteur:
-        <input
-          type="text"
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Tags:
-        <input
-          type="text"
-          value={tags}
-          onChange={(e) => setTags(e.target.value.split(","))}
-          placeholder="Séparez les tags par des virgules"
-          required
-        />
-      </label>
-      <div className="error-message">{errorMessage}</div>
-      <button type="submit" disabled={isPending}>
-        {isPending ? "En cours..." : "Publier"}
-      </button>
-    </form>
+    <div>
+      <div>
+        <h2>Créer un article</h2>
+        {errorMessage && <p>{errorMessage}</p>}
+        {isPending && <p>Envoi en cours...</p>}
+      </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Titre:
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Contenu:
+            <textarea
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Catégorie:
+            <select
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              required
+            >
+              <option value="news">News</option>
+              <option value="musique">Musique</option>
+              <option value="cinema">Cinéma</option>
+              <option value="serie">Série</option>
+              <option value="jeuxvideo">Jeux vidéo</option>
+              <option value="hightech">High Tech</option>
+            </select>
+          </label>
+          <label>
+            Image:
+            <input
+              type="file"
+              accept="image/*"
+              onChange={(e) => setImage(e.target.files[0])}
+              required
+            />
+          </label>
+          <label>
+            Auteur:
+            <input
+              type="text"
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Tags:
+            <input
+              type="text"
+              value={tags}
+              onChange={(e) => setTags(e.target.value.split(","))}
+              placeholder="Séparez les tags par des virgules"
+              required
+            />
+          </label>
+          <div className="error-message">{errorMessage}</div>
+          <button type="submit" disabled={isPending}>
+            {isPending ? "En cours..." : "Publier"}
+          </button>
+        </form>
+      </div>
+    </div>
   );
 };
 
